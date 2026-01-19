4ab90df31cb4337aea1cfc8bc273d08b94f84b60
quimetalkorea <63148575+quimetalkorea@users.noreply.github.com>
2026-01-19 12:43:04 +0900

    Update meat_search.py

--- meat_search.py
+++ meat_search.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Digitalmeat 실시간 견적", page_icon="🥩", 
 st.title("🥩 Digitalmeat 실시간 견적기")
 
 # --- 새로운 구글 시트 주소 적용 ---
-GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQHAoBVsnxWHcvK9vgurKTLo-Ly_C-DzxcJyrnqLQ9kKuk-bhPnOX2IwV3k1zjS5P3OWhIvC3TJ3v57/pub?output=csv"
+GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6WRSpWMH47AgEZQNyxgIewZeKGrouVPIANIfbXsCdhGGtF3AcjAQ1yQcUClQuvjtmvcjXwALyypam/pub?output=csv"
 
 @st.cache_data(ttl=30) # 30초마다 최신 데이터 확인
 def load_data():
