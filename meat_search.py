73980c4236ef3240a2aa036888a70f74a7e6207e
quimetalkorea <63148575+quimetalkorea@users.noreply.github.com>
2026-01-21 14:15:22 +0900

    Update meat_search.py

--- meat_search.py
+++ meat_search.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Digitalmeat 실시간 견적", page_icon="🥩", 
 st.title("🥩 Digitalmeat 실시간 견적기")
 
 # --- 구글 시트 주소 ---
-GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRLgM2vj52wClebWJJV7jqyghPytijDb9xYEgAGExjpziUnEBfegQfowjXrfxAJ_yg0MiEXsauCK-8z/pub?output=csv"
+GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3YJSsIlraRI6z-crCxfRxnJUmNd7_jwX4oyM6eskt_li7IuFobtkT0FnNE2ZMyBZbBjeh0kdPiuT-/pub?output=csv"
 
 @st.cache_data(ttl=10)
 def load_data():
